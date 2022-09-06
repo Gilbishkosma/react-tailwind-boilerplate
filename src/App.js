4127ae33b3c8231777d61467352312e85e161cc5
GilbishKosma <gilbeeshkosma15@gmail.com>
2022-09-06 19:41:25 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      Click Me 
+    </button>
+    </div>
+  );
+}
+
+export default App;
